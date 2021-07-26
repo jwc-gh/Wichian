0831fe5f27a21cbb047df3b5edca93936ff0b702
wichian <wichian@qq.cn>
2021-07-26 14:17:03 +0800

    添加Console、Winform、Wpf和单元测试项目

---
+++ Wichian.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Furion;
+using Furion.DependencyInjection;
+using Furion.RemoteRequest.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Wichian.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = Inject.Create();
+            services.AddRemoteRequest();
+            services.Build();
+
+            var helloservice = App.GetService<IHelloService>();
+            Console.WriteLine(helloservice.SayHello());
+
+            var baiducontent = "https://www.baidu.com".GetAsStringAsync().GetAwaiter().GetResult();
+            Console.WriteLine(baiducontent);
+        }
+    }
+
+    public interface IHelloService
+    {
+        string SayHello();
+    }
+
+    public class HelloService : IHelloService, ITransient
+    {
+        public string SayHello()
+        {
+            return "Hello Wichian";
+        }
+    }
+}
---
+++ Wichian.ConsoleApp/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Furion;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wichian.ConsoleApp
+{
+    public class Startup : AppStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDatabaseAccessor()
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
